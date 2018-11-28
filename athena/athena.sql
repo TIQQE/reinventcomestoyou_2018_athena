efa529741475c7aeea62ffbebe5e32a52f9e2daa
bni <bjorn.nilsson@tiqqe.com>
2018-11-28 11:31:44 +0100

    Some cleanups

--- athena/athena.sql
+++ athena/athena.sql
@@ -17,6 +17,7 @@ CREATE EXTERNAL TABLE demo.HistoricalEvents (
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 LOCATION 's3://reinventcomestoyou-test/processed/';
 
+-- Select everything, cusomt query to use in QuickSight
 SELECT
   CAST(SPLIT_PART(event.date, '/', 1) AS INTEGER) AS year,
   REPLACE(event.date, '/', '-') AS date,
@@ -28,7 +29,7 @@ SELECT
 FROM
   demo.HistoricalEvents;
 
-
+-- Find all events during certain years
 SELECT
   CAST(SPLIT_PART(event.date, '/', 1) AS INTEGER) AS year,
   REPLACE(event.date, '/', '-') AS date,
@@ -43,3 +44,18 @@ WHERE
   CAST(SPLIT_PART(event.date, '/', 1) AS INTEGER) BETWEEN 320 AND 350 AND
   event.category1 = 'By topic' AND
   event.category2 = 'Art';
+
+
+-- Example for a more complex query used in the field, to find turnover for a month
+SELECT
+  'Sept 2018' AS DATE_INTERVAL,
+  COUNT(DISTINCT cartId) AS NR_PURCHASES,
+  COUNT(1) AS NR_ORDER_ITEMS,
+  SUM(itemAmount)/100 AS TOTAL_SEK_AMOUNT
+FROM
+  NordicPayment.NordicPaymentCartItems
+WHERE
+  cartId LIKE 'ST%' AND
+  content.validFrom = 'SE' AND
+  paymentDate IS NOT NULL AND
+  paymentDate BETWEEN '2018-09-01' AND '2018-09-30';
--- test/transformRow.spec.js
+++ test/transformRow.spec.js
@@ -5,8 +5,6 @@ const assert = require("chai").assert;
 describe("transformRow", () => {
   it("Should transform row to a proper JSON object", (done) => {
     try {
-      //const record = "\"date\": \"1254\", \"description\": \"Some event\", \"lang\": \"en\", \"category1\": \"By place\", \"category2\": \"Egypt\", \"granularity\": \"year\"}";
-
       const record = `"date": "1254", "description": "Some event", "lang": "en", "category1": "By place", "category2": "Egypt", "granularity": "year"}`;
 
       const expected = {
